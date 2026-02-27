a56e5f4df28fb7a7bb97b4a9be2ca69807858802
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-02-27 12:03:53 +0000

    DAS-1587 Update print this page button

--- assets/js/printThisPage.js
+++ assets/js/printThisPage.js
@@ -10,3 +10,13 @@ const setupPrintThisPage = () => {
 }
 
 export default setupPrintThisPage;
+
+window.onload = function () {
+  var printLinks = document.querySelectorAll('.js-print-link');
+  printLinks.forEach(function (link) {
+    link.removeAttribute('hidden');
+    link.addEventListener('click', function () {
+      window.print();
+    });
+  });
+};
