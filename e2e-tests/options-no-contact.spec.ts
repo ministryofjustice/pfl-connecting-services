1118501c55317d333f426dd35c97a050f1b0ec62
Rahat-hassan-justice <rahat.hassan@justice.gov.uk>
2026-03-05 14:16:10 +0000

    DAS-1461-Update-Options-No-Contact.spec.ts
    Added CS flow

--- e2e-tests/options-no-contact.spec.ts
+++ e2e-tests/options-no-contact.spec.ts
@@ -1,33 +1,38 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.skip('Options no contact', () => {
-  test('should display the page with correct title', async ({ page }) => {
-    await page.goto('/options-no-contact');
+import { startJourney, selectDomesticAbuseOption, selectContactChildArrangementsOption } from './fixtures/test-helpers';
 
-    await expect(page).toHaveTitle("Options to explore if you are not comfortable contacting your ex-partner – Get help finding a child arrangement option – GOV.UK");
+test.beforeEach(async ({ page }) => {
+  await startJourney(page);
+  await selectDomesticAbuseOption(page, "No");
+  await selectContactChildArrangementsOption(page, 'No, I am not comfortable contacting them');
+});
+
+test.describe('Options no contact', () => {
+  test('should display the page with correct url and title', async ({ page }) => {
+    await expect(page).toHaveURL(/options-no-contact/);
+    await expect(page.locator('h1')).toContainText('Options to explore if you are not comfortable contacting your ex-partner');
+  });
+
+  test('should also navigate to options no contact when "I can contact them but they do not respond" is selected on Contact Child Arrangments page', async ({ page }) => {
+    await page.locator('a.govuk-back-link').click();
+    await selectContactChildArrangementsOption(page, 'I can contact them but they do not respond');
+    await expect(page).toHaveURL(/options-no-contact/);
+    await expect(page.locator('h1')).toContainText('Options to explore if you are not comfortable contacting your ex-partner');
   });
 
   test('should navigate to contact child arrangements page when back link is clicked', async ({ page }) => {
-    await page.goto('/options-no-contact');
-
     await page.locator('a.govuk-back-link').click();
-
-    await expect(page).toHaveURL(/\/contact-child-arrangements/);
+    await expect(page).toHaveURL(/contact-child-arrangements/);
   });
 
   test('should navigate to family mediation council website', async ({ page }) => {
-    await page.goto('/options-no-contact');
-
     await page.locator('#mediation-link').click();
-
     await expect(page).toHaveURL('https://www.familymediationcouncil.org.uk/');
   });
 
   test('should navigate to citizens advice website', async ({ page }) => {
-    await page.goto('/options-no-contact');
-
-    await page.locator('#citizens-advice-link').click();
-
+    await page.locator('#legal-advice-link').click();
     await expect(page).toHaveURL('https://www.citizensadvice.org.uk/family/making-agreements-about-your-children/your-child-arrangements-arent-working/');
   });
 });
