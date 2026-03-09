4cf3509a7003345c5906d5b694a000d576d6d15a
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-03-09 14:36:55 +0000

    Update unit test

--- server/middleware/setupAuthentication.test.ts
+++ server/middleware/setupAuthentication.test.ts
@@ -1,15 +1,12 @@
 import request from 'supertest';
 
 import config from '../config';
-import cookieNames from '../constants/cookieNames';
 import paths from '../constants/paths';
 import testAppSetup from '../test-utils/testAppSetup';
 
 // Local variables
 
 const testPassword = ['test', 'Password'].join('');
-const encryptedTestPassword = ['fd5cb51bafd60f6fdbedde6e62c473da6f247db271633e15919bab78a02ee9eb'].join('');
-const validAuthenticationCookie = `${cookieNames.AUTHENTICATION}=${encryptedTestPassword}`;
 const app = testAppSetup();
 
 describe('When a route is called', () => {
