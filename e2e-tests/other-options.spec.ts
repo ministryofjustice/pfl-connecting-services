c0bcb71f56a028630c20d2c1dd6c29f9302c83d1
Rahat-hassan-justice <rahat.hassan@justice.gov.uk>
2026-03-12 16:43:08 +0000

    Removed unused import

--- e2e-tests/other-options.spec.ts
+++ e2e-tests/other-options.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-import { startJourney, selectDomesticAbuseOption, selectContactChildArrangementsOption, selectAgreeOnChildArrangementsOption, selectHelpToAgreeOnChildArrangementsOption, selectOtherOptions} from './fixtures/test-helpers';
+import { startJourney, selectDomesticAbuseOption, selectContactChildArrangementsOption, selectAgreeOnChildArrangementsOption, selectHelpToAgreeOnChildArrangementsOption} from './fixtures/test-helpers';
 
 test.describe('Other options', () => {
 
