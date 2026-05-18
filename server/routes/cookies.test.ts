b0fb1c6cf6f79574e51f2266f910557988fb5bb7
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-05-18 10:06:37 +0100

    DAS-1974 Update cookie policy

--- server/routes/cookies.test.ts
+++ server/routes/cookies.test.ts
@@ -32,6 +32,17 @@ describe(paths.COOKIES, () => {
       expect(dom.window.document.querySelector('h1')).toHaveTextContent('Cookies');
       expect(dom.window.document.querySelector('fieldset')).not.toBeNull();
     });
+
+    it('should render OpenSearch analytics survey details when there is a ga4 id', async () => {
+      const response = await request(app).get(paths.COOKIES).expect('Content-Type', /html/);
+      const dom = new JSDOM(response.text);
+
+      const surveyHeading = Array.from(dom.window.document.querySelectorAll('h3')).find((heading) => heading.textContent?.trim() === 'Surveys (optional)');
+      expect(surveyHeading).not.toBeNull();
+      expect(response.text).toContain('govuk_taken[NameOfSurvey]');
+      expect(response.text).toContain('govuk_surveySeen[NameOfSurvey]');
+      expect(response.text).toContain('https://www.smartsurvey.co.uk/company/how-we-use-cookies');
+    });
   });
 
   describe('POST', () => {
@@ -63,6 +74,43 @@ describe(paths.COOKIES, () => {
             `_ga_${config.analytics.ga4Id}=; Domain=127.0.0.1; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
           ]);
         });
+    });
+
+    it('should set secure cookie when useHttps is enabled', async () => {
+      config.analytics.ga4Id = 'test-ga4-id';
+      config.useHttps = true;
+
+      await request(app)
+        .post(paths.COOKIES)
+        .send({ [ACCEPT_OPTIONAL_COOKIES]: 'Yes' })
+        .expect((response) => {
+          const cookieHeader = response.header['set-cookie'][0];
+          expect(cookieHeader).toContain('Secure');
+        });
+
+      config.useHttps = false;
+    });
+
+    it('should not clear analytics cookies when accepting analytics', async () => {
+      config.analytics.ga4Id = 'test-ga4-id';
+
+      await request(app)
+        .post(paths.COOKIES)
+        .send({ [ACCEPT_OPTIONAL_COOKIES]: 'Yes' })
+        .expect((response) => {
+          const cookieHeader = response.header['set-cookie'][0];
+          expect(cookieHeader).not.toContain('_ga=');
+        });
+    });
+
+    it('should include back link in page content', async () => {
+      const response = await request(app).get(paths.COOKIES).expect('Content-Type', /html/);
+
+      const dom = new JSDOM(response.text);
+      const backLink = dom.window.document.querySelector('a.govuk-back-link');
+
+      expect(backLink).not.toBeNull();
+      expect(backLink?.getAttribute('href')).toBe(config.serviceUrl);
     });
   });
 });
